81538f56020f38a56fb845ae553854c7744b51af
kdpandey09 <pandeykaustubdutt@gmail.com>
2021-11-29 19:38:59 +0530

    updated

--- YT_downloader.py
+++ YT_downloader.py
@@ -12,7 +12,7 @@ Label(root,text = 'Youtube Video Downloader', font ='arial 20 bold').pack()
 link = StringVar()
 Label(root, text = 'Paste Link Here:', font = 'arial 15 bold').place(x= 160 , y = 60)
 link_enter = Entry(root, width = 70,textvariable = link).place(x = 32, y = 90)
-
+#updated
 def Downloader():     
     url =YouTube(str(link.get()))
     video = url.streams.first()
