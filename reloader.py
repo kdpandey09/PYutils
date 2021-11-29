071010bf39db609262c8f7bf58b362179021f553
kdpandey09 <pandeykaustubdutt@gmail.com>
2021-11-30 00:35:51 +0530

    comments added

--- reloader.py
+++ reloader.py
@@ -1,11 +1,13 @@
 import pyautogui as pg
 import time
 import webbrowser as wb
+# user input
 url= str(input("Enter the link of the site you want to reload \n"))
 number = int(input("Enter how many times you want it to reload \n"))
-print('hello')
+# opening the website via your default browser
 wb.open(url)
 time.sleep(5)
+# loop for multiple reloding 
 for i in range (number):
     time.sleep(4)
     pg.hotkey('ctrl', 'r')
