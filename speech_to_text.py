5649c0f9dbc612de32bb9ec5bf514d9cba410543
kdpandey09 <pandeykaustubdutt@gmail.com>
2021-12-03 00:48:51 +0530

    updated message pop up and timeout

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,11 +1,10 @@
 import speech_recognition as s
 r = s.Recognizer()
 print("I am Listening you....")
-# under work
 
 with s.Microphone() as source:
-    audio = r.listen(source)
+    audio = r.listen(source, 10)
 
     message = r.recognize_google(audio,language='eng-in', show_all =True)
 
-    print(message)
+    print("Your Transcripted Message is: ",message['alternative'][0]['transcript']
